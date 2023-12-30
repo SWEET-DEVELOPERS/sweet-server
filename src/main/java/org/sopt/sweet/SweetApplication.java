ea345de9bcc8312c4c8a0238b9abc2f9ec3ad34f
ziiyouth <ziy913@naver.com>
2023-12-31 02:29:58 +0900

    first commit

---
+++ src/main/java/org/sopt/sweet/SweetApplication.java
@@ -0,0 +1,13 @@
+package org.sopt.sweet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SweetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SweetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/sopt/sweet/SweetApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sopt.sweet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SweetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
