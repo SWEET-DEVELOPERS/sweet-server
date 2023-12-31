11ced983f50437a1d3f4e153b7f8205a11a4934d
ziiyouth <ziy913@naver.com>
2023-12-31 14:51:35 +0900

    [feat] #1 business exception 구현

---
+++ src/main/java/org/sopt/sweet/global/error/exception/BusinessException.java
@@ -0,0 +1,19 @@
+package org.sopt.sweet.global.error.exception;
+
+import lombok.Getter;
+import org.sopt.sweet.global.error.ErrorCode;
+
+@Getter
+public class BusinessException extends RuntimeException {
+    private final ErrorCode errorCode;
+
+    public BusinessException(String message, ErrorCode errorCode) {
+        super(message);
+        this.errorCode = errorCode;
+    }
+
+    public BusinessException(ErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.errorCode = errorCode;
+    }
+}
