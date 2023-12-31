059fc30849216d3b691ffcb14dca17f993e5d938
ziiyouth <ziy913@naver.com>
2023-12-31 14:48:41 +0900

    [feat] #1 user authentication 등록

---
+++ src/main/java/org/sopt/sweet/global/config/auth/UserId.java
@@ -0,0 +1,12 @@
+package org.sopt.sweet.global.config.auth;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.PARAMETER)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface UserId {
+}
+
