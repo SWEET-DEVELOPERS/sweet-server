e7c0f5596723d9b42b603f59e54dc42348fc1b69
ziiyouth <ziy913@naver.com>
2023-12-31 14:52:47 +0900

    [feat] #1 invalid value exception 구현

---
+++ src/main/java/org/sopt/sweet/global/error/exception/InvalidValueException.java
@@ -0,0 +1,13 @@
+package org.sopt.sweet.global.error.exception;
+
+import org.sopt.sweet.global.error.ErrorCode;
+
+public class InvalidValueException extends BusinessException {
+    public InvalidValueException() {
+        super(ErrorCode.BAD_REQUEST);
+    }
+
+    public InvalidValueException(ErrorCode errorCode) {
+        super(errorCode);
+    }
+}
