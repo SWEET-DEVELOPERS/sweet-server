808dadff73df15b46aef2a9a50feaddb96650475
ziiyouth <ziy913@naver.com>
2023-12-31 14:50:55 +0900

    [feat] #1 exception handler filter 구현

---
+++ src/main/java/org/sopt/sweet/global/config/auth/ExceptionHandlerFilter.java
@@ -0,0 +1,51 @@
+package org.sopt.sweet.global.config.auth;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.sopt.sweet.global.error.ErrorCode;
+import org.sopt.sweet.global.error.dto.ErrorResponse;
+import org.sopt.sweet.global.error.exception.InvalidValueException;
+import org.sopt.sweet.global.error.exception.UnauthorizedException;
+import org.springframework.http.MediaType;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Slf4j
+public class ExceptionHandlerFilter extends OncePerRequestFilter {
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        try {
+            filterChain.doFilter(request, response);
+        } catch (UnauthorizedException e) {
+            handleUnauthorizedException(response, e);
+        } catch (Exception ee) {
+            handleException(response);
+        }
+    }
+
+    private void handleUnauthorizedException(HttpServletResponse response, Exception e) throws IOException {
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setCharacterEncoding("utf-8");
+        if (e instanceof UnauthorizedException ue) {
+            response.setStatus(ue.getErrorCode().getHttpStatus().value());
+            response.getWriter().write(objectMapper.writeValueAsString(ErrorResponse.of(ue.getErrorCode())));
+        } else if (e instanceof InvalidValueException ie) {
+            response.setStatus(ie.getErrorCode().getHttpStatus().value());
+            response.getWriter().write(objectMapper.writeValueAsString(ErrorResponse.of(ie.getErrorCode())));
+        }
+    }
+
+    private void handleException(HttpServletResponse response) throws IOException {
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setCharacterEncoding("utf-8");
+        response.setStatus(ErrorCode.INTERNAL_SERVER_ERROR.getHttpStatus().value());
+        response.getWriter().write(objectMapper.writeValueAsString(ErrorResponse.of(ErrorCode.INTERNAL_SERVER_ERROR)));
+    }
+}
