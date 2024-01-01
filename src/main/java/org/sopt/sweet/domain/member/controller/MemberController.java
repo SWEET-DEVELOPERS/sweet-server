6859e02500fac50d2598b649fb40346c380d3bc4
ziiyouth <ziy913@naver.com>
2024-01-01 21:24:57 +0900

    [feat] #5 Member controller 생성

---
+++ src/main/java/org/sopt/sweet/domain/member/controller/MemberController.java
@@ -0,0 +1,10 @@
+package org.sopt.sweet.domain.member.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/api/member")
+@RestController
+public class MemberController {
+
+}
