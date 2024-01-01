c645dde548a066e7e8ec9b6b55678d5446448e91
ziiyouth <ziy913@naver.com>
2024-01-01 21:25:41 +0900

    [feat] #5 Member service 생성

---
+++ src/main/java/org/sopt/sweet/domain/member/service/MemberService.java
@@ -0,0 +1,9 @@
+package org.sopt.sweet.domain.member.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class MemberService {
+}
