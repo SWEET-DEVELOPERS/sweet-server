b594ada66285f1b543f1209bc0a062192adb265c
ziiyouth <ziy913@naver.com>
2024-01-02 00:24:47 +0900

    [feat] #9 임시 로그인 API 생성

--- src/main/java/org/sopt/sweet/domain/member/controller/MemberController.java
+++ src/main/java/org/sopt/sweet/domain/member/controller/MemberController.java
@@ -1,18 +1,29 @@
 package org.sopt.sweet.domain.member.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.sopt.sweet.domain.member.dto.request.MemberTokenResponseDto;
+import org.sopt.sweet.domain.member.service.MemberService;
 import org.sopt.sweet.global.common.SuccessResponse;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@RequiredArgsConstructor
 @RequestMapping("/api/member")
 @RestController
 public class MemberController implements MemberApi{
+
+    private final MemberService memberService;
 
     @GetMapping(path = "/test")
     public ResponseEntity<SuccessResponse<?>> testSwagger() {
         String response = "Sweet little kitty";
         return SuccessResponse.ok(response);
+    }
+
+    // 임시 발급 API 입니다. 추후 로그인 기능이 완성되면 삭제할 예정입니다.
+    @PostMapping("/token/{memberId}")
+    public ResponseEntity<SuccessResponse<?>> getToken(@PathVariable Long memberId){
+        final MemberTokenResponseDto memberTokenResponseDto = memberService.getToken(memberId);
+        return SuccessResponse.created(memberTokenResponseDto);
     }
 }
