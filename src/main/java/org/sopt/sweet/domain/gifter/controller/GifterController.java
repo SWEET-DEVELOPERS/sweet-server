859e9f0638ba5b9ff9e77ca76218cb3cfcd7103d
ziiyouth <ziy913@naver.com>
2024-01-02 00:25:05 +0900

    [feat] #9 @RequiredArgsConstructor 어노테이션 추가

--- src/main/java/org/sopt/sweet/domain/gifter/controller/GifterController.java
+++ src/main/java/org/sopt/sweet/domain/gifter/controller/GifterController.java
@@ -1,8 +1,10 @@
 package org.sopt.sweet.domain.gifter.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequiredArgsConstructor
 @RequestMapping("/api/gifter")
 @RestController
 public class GifterController implements GifterApi {
