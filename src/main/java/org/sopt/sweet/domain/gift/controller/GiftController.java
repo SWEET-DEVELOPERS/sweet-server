fe741fce711fb938f16ad55b46613988d44c017c
ziiyouth <ziy913@naver.com>
2024-01-01 21:24:33 +0900

    [feat] #5 Gift controller 생성

---
+++ src/main/java/org/sopt/sweet/domain/gift/controller/GiftController.java
@@ -0,0 +1,9 @@
+package org.sopt.sweet.domain.gift.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/api/gift")
+@RestController
+public class GiftController {
+}
