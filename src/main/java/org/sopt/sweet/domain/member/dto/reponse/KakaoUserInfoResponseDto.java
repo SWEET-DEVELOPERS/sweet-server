bdca48dedd8197a1939b265b54825a18b4fbe496
hysong4u <hayeon_song@naver.com>
2024-01-07 04:09:09 +0900

    [feat] #11 KakaoUserInfoResponseDto 구현

---
+++ src/main/java/org/sopt/sweet/domain/member/dto/reponse/KakaoUserInfoResponseDto.java
@@ -0,0 +1,29 @@
+package org.sopt.sweet.domain.member.dto.reponse;
+
+
+import org.sopt.sweet.domain.member.entity.Member;
+import org.sopt.sweet.domain.member.entity.OAuthToken;
+
+
+public record KakaoUserInfoResponseDto(
+        Long socialId,
+        String nickname,
+        String profileImage,
+        String accessToken,
+        String refreshToken
+) {
+
+
+    KakaoUserInfoResponseDto of(Member member, OAuthToken oAuthToken) {
+        return new KakaoUserInfoResponseDto(
+                member.getSocialId(),
+                member.getNickName(),
+                member.getProfileImg(),
+                oAuthToken.getAccess_token(),
+                oAuthToken.getRefresh_token()
+        );
+    }
+
+
+}
+
