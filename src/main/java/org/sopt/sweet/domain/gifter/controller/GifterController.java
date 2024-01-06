ba561c27654c8dee3ca272a4a663303685ec28c0
ziiyouth <ziy913@naver.com>
2024-01-07 00:36:48 +0900

    [feat] #14 presigned URL controller 구현

--- src/main/java/org/sopt/sweet/domain/gifter/controller/GifterController.java
+++ src/main/java/org/sopt/sweet/domain/gifter/controller/GifterController.java
@@ -1,6 +1,13 @@
 package org.sopt.sweet.domain.gifter.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.sopt.sweet.domain.gifter.dto.request.PresignedURLRequestDto;
+import org.sopt.sweet.domain.gifter.dto.response.PresignedURLResponseDto;
+import org.sopt.sweet.domain.gifter.service.GifterService;
+import org.sopt.sweet.global.common.SuccessResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +15,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/gifter")
 @RestController
 public class GifterController implements GifterApi {
+
+    private final GifterService gifterService;
+
+    @PostMapping("/presignedURL")
+    public ResponseEntity<SuccessResponse<?>> getPresignedURL(@RequestBody PresignedURLRequestDto presignedURLRequestDto) {
+        final PresignedURLResponseDto presignedURLResponseDto = gifterService.getPresignedURL(presignedURLRequestDto);
+        return SuccessResponse.created(presignedURLResponseDto);
+    }
 }
