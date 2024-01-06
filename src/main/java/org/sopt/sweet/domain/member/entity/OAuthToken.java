cf24c244a34b2964c7240c83b4bbff81e0f764e8
hysong4u <hayeon_song@naver.com>
2024-01-07 04:10:13 +0900

    [feat] #11 OAuthToken 클래스 구현

---
+++ src/main/java/org/sopt/sweet/domain/member/entity/OAuthToken.java
@@ -0,0 +1,15 @@
+package org.sopt.sweet.domain.member.entity;
+
+import lombok.Data;
+import lombok.Getter;
+
+@Getter
+@Data
+public class OAuthToken {
+    private String access_token;
+    private String token_type;
+    private String refresh_token;
+    private int expires_in;
+    private String scope;
+    private int refresh_token_expires_in;
+}
