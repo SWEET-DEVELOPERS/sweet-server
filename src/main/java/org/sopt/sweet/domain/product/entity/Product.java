8dc530b61b7c1dd206a389b08ae004756f84d1b7
ziiyouth <ziy913@naver.com>
2024-01-07 23:07:30 +0900

    [refactor] #20 product 엔티티 빌더 패턴 적용 방식 변경

--- src/main/java/org/sopt/sweet/domain/product/entity/Product.java
+++ src/main/java/org/sopt/sweet/domain/product/entity/Product.java
@@ -6,8 +6,6 @@ import org.sopt.sweet.global.common.BaseTimeEntity;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@AllArgsConstructor(access = AccessLevel.PRIVATE)
-@Builder(access = AccessLevel.PRIVATE)
 @Table(name = "product")
 @Entity
 public class Product extends BaseTimeEntity {
