2b7df643ba066f2abc8dcbb308a0b9bb5f7e0c2a
ziiyouth <ziy913@naver.com>
2024-01-08 04:01:17 +0900

    [feat] #23 선물방 초대 조회 controller 구현

--- src/main/java/org/sopt/sweet/domain/room/controller/RoomController.java
+++ src/main/java/org/sopt/sweet/domain/room/controller/RoomController.java
@@ -2,14 +2,12 @@ package org.sopt.sweet.domain.room.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.sopt.sweet.domain.room.dto.request.CreateRoomRequestDto;
+import org.sopt.sweet.domain.room.dto.response.RoomInviteResponseDto;
 import org.sopt.sweet.domain.room.service.RoomService;
 import org.sopt.sweet.global.common.SuccessResponse;
 import org.sopt.sweet.global.config.auth.UserId;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RequestMapping("/api/room")
@@ -23,6 +21,12 @@ public class RoomController implements RoomApi {
                                                             @RequestBody CreateRoomRequestDto createRoomRequestDto){
         final Long roomId = roomService.createNewRoom(userId, createRoomRequestDto);
         return SuccessResponse.created(roomId);
+    }
+
+    @GetMapping("/invitations/{roomId}")
+    public ResponseEntity<SuccessResponse<?>> getRoomInviteInfo(@PathVariable Long roomId){
+        final RoomInviteResponseDto roomInviteResponseDto = roomService.getRoomInviteInfo(roomId);
+        return SuccessResponse.ok(roomInviteResponseDto);
     }
 
 }
