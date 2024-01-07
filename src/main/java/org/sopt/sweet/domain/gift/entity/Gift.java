2a21c9969ce896f39c21354cdb3f3d26a97bcb79
ziiyouth <ziy913@naver.com>
2024-01-07 23:04:44 +0900

    [refactor] #20 gift 엔티티 빌더 패턴 적용 방식 변경과 DEFAULT_SCORE 설정

--- src/main/java/org/sopt/sweet/domain/gift/entity/Gift.java
+++ src/main/java/org/sopt/sweet/domain/gift/entity/Gift.java
@@ -2,23 +2,22 @@ package org.sopt.sweet.domain.gift.entity;
 
 import jakarta.persistence.*;
 import lombok.*;
-import org.sopt.sweet.domain.gifter.entity.Gifter;
+import org.sopt.sweet.domain.room.entity.Room;
 import org.sopt.sweet.domain.member.entity.Member;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@AllArgsConstructor(access = AccessLevel.PRIVATE)
-@Builder(access = AccessLevel.PRIVATE)
 @Table(name = "gift")
 @Entity
 public class Gift {
+
+    private static final int DEFAULT_SCORE = 0;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "gift_id")
     private Long id;
 
-    @Column(nullable = false)
     private String url;
 
     @Column(nullable = false)
@@ -29,14 +28,24 @@ public class Gift {
 
     private String img;
 
-    @Builder.Default
-    private int score = 0;
+    private int score;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "gifter_id")
-    private Gifter gifter;
+    @JoinColumn(name = "room_id")
+    private Room room;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id", nullable = false)
     private Member member;
+
+    @Builder
+    public Gift(String url, String name, int cost, String img, Room room, Member member) {
+        this.url = url;
+        this.name = name;
+        this.cost = cost;
+        this.score = DEFAULT_SCORE;
+        this.img = img;
+        this.room = room;
+        this.member = member;
+    }
 }
