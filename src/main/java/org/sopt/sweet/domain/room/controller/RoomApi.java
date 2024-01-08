bad018e9e00780e46a013e43478b031564a244bf
ziiyouth <ziy913@naver.com>
2024-01-09 02:14:41 +0900

    [feat] #23 선물방 참여 기능 swagger 코드 구현

--- src/main/java/org/sopt/sweet/domain/room/controller/RoomApi.java
+++ src/main/java/org/sopt/sweet/domain/room/controller/RoomApi.java
@@ -10,6 +10,7 @@ import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.sopt.sweet.domain.room.dto.request.CreateRoomRequestDto;
+import org.sopt.sweet.domain.room.dto.request.JoinRoomRequestDto;
 import org.sopt.sweet.global.common.SuccessResponse;
 import org.sopt.sweet.global.config.auth.UserId;
 import org.springframework.http.ResponseEntity;
@@ -59,5 +60,27 @@ public interface RoomApi {
                     example = "1"
             ) @PathVariable String invitationCode
     );
+
+    @Operation(
+            summary = "선물방 참여 API",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = SuccessResponse.class)
+                            )
+                    )
+            },
+            security = @SecurityRequirement(name = "token")
+    )
+    ResponseEntity<SuccessResponse<?>> joinRoom(
+            @Parameter(
+                    description = "authorization token에서 얻은 userId, 임의입력하면 대체됩니다.",
+                    required = true,
+                    example = "12345"
+            ) @UserId Long userId,
+            @Valid @RequestBody JoinRoomRequestDto joinRoomRequestDto
+            );
 
 }
