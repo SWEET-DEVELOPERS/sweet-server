9c9b45fc932c549f74d0109336c67f1c0ae3cb63
ziiyouth <ziy913@naver.com>
2024-01-09 13:18:56 +0900

    [feat] #25 내가 등록한 선물 조회 기능 controller 코드 구현

--- src/main/java/org/sopt/sweet/domain/gift/controller/GiftController.java
+++ src/main/java/org/sopt/sweet/domain/gift/controller/GiftController.java
@@ -2,14 +2,13 @@ package org.sopt.sweet.domain.gift.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.sopt.sweet.domain.gift.dto.request.CreateGiftRequestDto;
+import org.sopt.sweet.domain.gift.dto.request.MyGiftsRequestDto;
+import org.sopt.sweet.domain.gift.dto.response.MyGiftsResponseDto;
 import org.sopt.sweet.domain.gift.service.GiftService;
 import org.sopt.sweet.global.common.SuccessResponse;
 import org.sopt.sweet.global.config.auth.UserId;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RequestMapping("/api/gift")
@@ -22,5 +21,11 @@ public class GiftController implements GiftApi {
     public ResponseEntity<SuccessResponse<?>> createNewGift(@UserId Long userId, @RequestBody CreateGiftRequestDto createGiftRequestDto) {
         giftService.createNewGift(userId, createGiftRequestDto);
         return SuccessResponse.created(null);
+    }
+
+    @GetMapping("/my")
+    public ResponseEntity<SuccessResponse<?>> getMyGift(@UserId Long userId, @RequestBody MyGiftsRequestDto myGiftsRequestDto) {
+        final MyGiftsResponseDto myGiftsResponseDto = giftService.getMyGift(userId, myGiftsRequestDto);
+        return SuccessResponse.ok(myGiftsResponseDto);
     }
 }
