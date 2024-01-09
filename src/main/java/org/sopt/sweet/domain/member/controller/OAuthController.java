7e3f1b063b3bb4ed6aaa0bfc49bb9c672df55f6c
hysong4u <hayeon_song@naver.com>
2024-01-10 05:23:51 +0900

    [feat] #11 카카오 로그아웃 controller 구현

--- src/main/java/org/sopt/sweet/domain/member/controller/OAuthController.java
+++ src/main/java/org/sopt/sweet/domain/member/controller/OAuthController.java
@@ -6,10 +6,7 @@ import org.sopt.sweet.domain.member.dto.reponse.MemberTokenResponseDto;
 import org.sopt.sweet.domain.member.service.OAuthService;
 import org.sopt.sweet.global.common.SuccessResponse;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -31,6 +28,13 @@ public class OAuthController implements OAuthApi {
         loginResponse.put("memberToken", memberToken);
 
         return SuccessResponse.ok(loginResponse);
+    }
+
+
+    @PostMapping("/kakao/logout")
+    public ResponseEntity<SuccessResponse<?>> kakaoLogout(@RequestHeader("Authorization") String accessToken,@RequestBody String socialId) {
+        oAuthService.kakaoLogout(accessToken, socialId);
+        return SuccessResponse.ok("로그아웃 성공");
     }
 
 
