6b6ec53b261a859d8b2ea0a833615b1c2b61d2ab
ziiyouth <ziy913@naver.com>
2024-01-10 01:55:41 +0900

    [feat] #29 선물방 받는 사람 이름 편집 기능 swagger code 구현

--- src/main/java/org/sopt/sweet/domain/room/controller/RoomApi.java
+++ src/main/java/org/sopt/sweet/domain/room/controller/RoomApi.java
@@ -12,6 +12,7 @@ import jakarta.validation.Valid;
 import org.sopt.sweet.domain.room.dto.request.CreateRoomRequestDto;
 import org.sopt.sweet.domain.room.dto.request.JoinRoomRequestDto;
 import org.sopt.sweet.domain.room.dto.request.RoomImageRequestDto;
+import org.sopt.sweet.domain.room.dto.request.RoomNameRequestDto;
 import org.sopt.sweet.global.common.SuccessResponse;
 import org.sopt.sweet.global.config.auth.UserId;
 import org.springframework.http.ResponseEntity;
@@ -137,7 +138,7 @@ public interface RoomApi {
     );
 
     @Operation(
-            summary = "선물방 사진 수정 API",
+            summary = "선물방 썸네일 수정 API",
             responses = {
                     @ApiResponse(
                             responseCode = "200",
@@ -161,6 +162,33 @@ public interface RoomApi {
                     example = "1"
             ) @PathVariable Long roomId,
             @RequestBody RoomImageRequestDto roomImageRequestDto
+    );
+
+    @Operation(
+            summary = "선물방 이름 수정 API",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = SuccessResponse.class)
+                            )
+                    )
+            },
+            security = @SecurityRequirement(name = "token")
+    )
+    ResponseEntity<SuccessResponse<?>> modifyRoomGifteeName(
+            @Parameter(
+                    description = "authorization token에서 얻은 userId, 임의입력하면 대체됩니다.",
+                    required = true,
+                    example = "12345"
+            ) @UserId Long userId,
+            @Parameter(
+                    description = "room 고유 id",
+                    required = true,
+                    example = "1"
+            ) @PathVariable Long roomId,
+            @RequestBody RoomNameRequestDto roomNameRequestDto
     );
 
 }
