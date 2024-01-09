880822065ebc29f1ff15efad0a324be98b1d2dc7
ziiyouth <ziy913@naver.com>
2024-01-09 18:37:34 +0900

    [feat] #27 선물방 메인 조회 기능 service 구현

--- src/main/java/org/sopt/sweet/domain/gift/repository/GiftRepository.java
+++ src/main/java/org/sopt/sweet/domain/gift/repository/GiftRepository.java
@@ -3,11 +3,18 @@ package org.sopt.sweet.domain.gift.repository;
 import org.sopt.sweet.domain.gift.entity.Gift;
 import org.sopt.sweet.domain.member.entity.Member;
 import org.sopt.sweet.domain.room.entity.Room;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface GiftRepository extends JpaRepository<Gift, Long> {
     long countByRoomAndMember(Room room, Member member);
+
     List<Gift> findByRoomAndMember(Room room, Member member);
+
+    @Query("SELECT g FROM Gift g WHERE g.room = :room AND g.member <> :member ORDER BY g.id DESC")
+    List<Gift> findLatestGiftsByRoomAndNotMember(@Param("room") Room room, @Param("member") Member member, Pageable pageable);
 }
