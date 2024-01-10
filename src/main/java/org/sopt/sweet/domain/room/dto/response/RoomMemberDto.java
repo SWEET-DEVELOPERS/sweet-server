0d33219f2c0a1f8e6340e6a01a5285254bffd285
ziiyouth <ziy913@naver.com>
2024-01-10 16:15:38 +0900

    [feat] #33 선물방 참여자 목록 조회 dto 구현

---
+++ src/main/java/org/sopt/sweet/domain/room/dto/response/RoomMemberDto.java
@@ -0,0 +1,18 @@
+package org.sopt.sweet.domain.room.dto.response;
+
+import lombok.Builder;
+
+@Builder
+public record RoomMemberDto(
+        Long memberId,
+        String profileImageUrl,
+        String name
+) {
+    public static RoomMemberDto of(Long memberId, String profileImageUrl, String name){
+        return RoomMemberDto.builder()
+                .memberId(memberId)
+                .profileImageUrl(profileImageUrl)
+                .name(name)
+                .build();
+    }
+}
