ff19134f4aadeb5ff90da4ac5c3e7a34c0342f04
hysong4u <hayeon_song@naver.com>
2024-01-12 19:13:05 +0900

    [feat] 토너먼트 점수 등록 API swagger 구현

--- src/main/java/org/sopt/sweet/domain/gift/controller/GiftApi.java
+++ src/main/java/org/sopt/sweet/domain/gift/controller/GiftApi.java
@@ -11,6 +11,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.sopt.sweet.domain.gift.dto.request.CreateGiftRequestDto;
 import org.sopt.sweet.domain.gift.dto.request.MyGiftsRequestDto;
+import org.sopt.sweet.domain.gift.dto.request.TournamentScoreRequestDto;
 import org.sopt.sweet.global.common.SuccessResponse;
 import org.sopt.sweet.global.config.auth.UserId;
 import org.springframework.http.ResponseEntity;
@@ -122,4 +123,25 @@ public interface GiftApi {
             ) @PathVariable Long roomId
     );
 
+    @Operation(
+            summary = "선물 토너먼트 점수 등록 API",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "201",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = SuccessResponse.class)
+                            )
+                    )
+            },
+            security = @SecurityRequirement(name = "token")
+    )
+    ResponseEntity<SuccessResponse<?>> evaluateTournamentScore(
+            @Parameter(
+                    description = "authorization token에서 얻은 userId, 임의입력하면 대체됩니다.",
+                    required = true,
+                    example = "12345"
+            ) @UserId Long userId,
+            @Valid @RequestBody TournamentScoreRequestDto tournamentScoreRequestDto
+            );
 }
