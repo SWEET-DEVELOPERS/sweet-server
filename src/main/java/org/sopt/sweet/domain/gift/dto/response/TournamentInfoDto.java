a7dc373df08794581c1a2016fd6b73afec2f2678
hysong4u <hayeon_song@naver.com>
2024-01-12 23:10:52 +0900

    [feat] 토너먼트 관련 정보 추출 API Dto 구현

---
+++ src/main/java/org/sopt/sweet/domain/gift/dto/response/TournamentInfoDto.java
@@ -0,0 +1,28 @@
+package org.sopt.sweet.domain.gift.dto.response;
+
+import lombok.Builder;
+import org.sopt.sweet.domain.room.constant.TournamentDuration;
+
+import java.time.LocalDateTime;
+@Builder
+public record TournamentInfoDto(
+        LocalDateTime tournamentStartDate,
+        TournamentDuration tournamentDuration,
+
+        int TotalParticipantsCount,
+        int ParticipantsCount
+
+) {
+    public static TournamentInfoDto of(LocalDateTime tournamentStartDate,
+                                       TournamentDuration tournamentDuration,
+                                       int TotalParticipantsCount,
+                                       int ParticipantsCount) {
+       return TournamentInfoDto.builder()
+               .tournamentStartDate(tournamentStartDate)
+               .tournamentDuration(tournamentDuration)
+               .TotalParticipantsCount(TotalParticipantsCount)
+               .ParticipantsCount(ParticipantsCount)
+               .build();
+    }
+
+}
