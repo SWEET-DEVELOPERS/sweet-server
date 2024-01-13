80ddf63f3207a3c19620e7d131516139905634e6
hysong4u <hayeon_song@naver.com>
2024-01-13 17:50:25 +0900

    [feat] 진행중인 선물방 API service 구현

--- src/main/java/org/sopt/sweet/domain/member/service/MemberService.java
+++ src/main/java/org/sopt/sweet/domain/member/service/MemberService.java
@@ -1,8 +1,10 @@
 package org.sopt.sweet.domain.member.service;
 
 import lombok.RequiredArgsConstructor;
+import org.sopt.sweet.domain.member.dto.response.ActiveRoomResponseDto;
 import org.sopt.sweet.domain.member.dto.response.ClosedRoomResponseDto;
 import org.sopt.sweet.domain.member.dto.response.MemberTokenResponseDto;
+import org.sopt.sweet.domain.member.entity.Member;
 import org.sopt.sweet.domain.member.repository.MemberRepository;
 import org.sopt.sweet.domain.room.entity.Room;
 import org.sopt.sweet.domain.room.entity.RoomMember;
@@ -15,6 +17,7 @@ import org.springframework.transaction.annotation.Transactional;
 import java.time.LocalDateTime;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
@@ -65,5 +68,37 @@ public class MemberService {
     }
 
 
+    public List<ActiveRoomResponseDto> getActiveRoom(Long memberId) {
+        List<RoomMember> roomMembers = roomMemberRepository.findByMemberId(memberId);
+        List<ActiveRoomResponseDto> activeRooms = roomMembers.stream()
+                .map(RoomMember::getRoom)
+                .filter(room -> room.getDeliveryDate().isAfter(LocalDateTime.now()))
+                .sorted(Comparator.comparing(room -> getRoomMemberCreationTime(room, memberId), Comparator.reverseOrder()))
+                .map(room -> mapToActiveRoomResponseDto(room, memberId))
+                .collect(Collectors.toList());
+        return activeRooms;
+    }
+
+    private ActiveRoomResponseDto mapToActiveRoomResponseDto(Room room, Long memberId) {
+        return new ActiveRoomResponseDto(
+                room.getId(),
+                room.getImageUrl(),
+                room.getGifteeName(),
+                room.getGifterNumber(),
+                room.getTournamentStartDate(),
+                isOwner(memberId, room.getId())
+        );
+    }
+    private LocalDateTime getRoomMemberCreationTime(Room room, Long memberId) {
+        Optional<RoomMember> roomMember = roomMemberRepository.findByMemberIdAndRoom(memberId, room);
+        return roomMember.map(RoomMember::getCreateDate).orElse(LocalDateTime.MIN);
+    }
+
+    public Boolean isOwner(Long memberId, Long roomId) {
+        Optional<Member> member = memberRepository.findById(memberId);
+        Optional<Room> room = roomRepository.findById(roomId);
+        Boolean isOwner = member.get().getId().equals(room.get().getHost().getId());
+        return isOwner;
+    }
 
 }
