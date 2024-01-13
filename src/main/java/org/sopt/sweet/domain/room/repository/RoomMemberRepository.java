33bf8c8d07829a66e4b5928b4be542d1032a3887
hysong4u <hayeon_song@naver.com>
2024-01-13 17:50:55 +0900

    [feat] 진행중인 선물방 API repository 구현

--- src/main/java/org/sopt/sweet/domain/room/repository/RoomMemberRepository.java
+++ src/main/java/org/sopt/sweet/domain/room/repository/RoomMemberRepository.java
@@ -16,4 +16,6 @@ public interface RoomMemberRepository extends JpaRepository<RoomMember, Long> {
     RoomMember findByRoomIdAndMemberId(Long roomId, Long memberId);
 
     List<RoomMember> findByMemberId(Long memberId);
+
+    Optional<RoomMember> findByMemberIdAndRoom(Long memberId, Room room);
 }
