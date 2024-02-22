bb578204a4cd44c104d07a7dc739de6d7b901835
hysong4u <hayeon_song@naver.com>
2024-02-23 01:56:02 +0900

    [fix] giftee name response 추가

--- src/main/java/org/sopt/sweet/domain/gift/dto/response/RoomInfoResponseDto.java
+++ src/main/java/org/sopt/sweet/domain/gift/dto/response/RoomInfoResponseDto.java
@@ -2,7 +2,8 @@ package org.sopt.sweet.domain.gift.dto.response;
 
 import java.time.LocalDateTime;
 
-public record TournamentStartDateResponseDto(
+public record RoomInfoResponseDto(
+        String gifteeName,
         LocalDateTime tournamentStartDate
 ) {
 }
